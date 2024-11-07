a6fff0f738b16b652be4141f6ba71bb87e7e207d
IM-GYURI <kgulr0517@gmail.com>
2024-11-08 00:47:17 +0900

    feat : Product의 각 필드에 대한 getter를 구현한다.

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -8,13 +8,25 @@ public class Product {
     private Promotion promotion;
 
     public Product(String name, int price, int quantity, Promotion promotion) {
-        // 검증 로직 추가
-
         this.name = name;
         this.price = price;
         this.quantity = quantity;
         this.promotion = promotion;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Promotion getPromotion() {
+        return promotion;
+    }
 }
