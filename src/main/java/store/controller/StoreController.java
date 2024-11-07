70e99b5eaa76f8740cf9294ede061e0cc1569976
IM-GYURI <kgulr0517@gmail.com>
2024-11-08 01:37:53 +0900

    refactor : StoreController가 InputView와 OutputView를 처리하는 View를 통해 입출력을 관리하도록 한다.

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,33 +1,26 @@
 package store.controller;
 
-import java.util.List;
-import store.domain.Product;
+import store.domain.Products;
 import store.service.StoreService;
-import store.view.OutputView;
+import store.view.View;
 
 public class StoreController {
 
     private static final String PRODUCT_FILE_PATH = "src/main/resources/products.md";
 
-    private final OutputView outputView;
+    private final View view;
     private final StoreService storeService;
 
-    public StoreController(OutputView outputView, StoreService storeService) {
-        this.outputView = outputView;
+    public StoreController(final View view, final StoreService storeService) {
+        this.view = view;
         this.storeService = storeService;
     }
 
     public void buyProducts() {
-        printStartStatement();
-        List<Product> products = storeService.loadProductsFromFile(PRODUCT_FILE_PATH);
-        printCurrentProducts(products);
-    }
+        view.printHello();
+        Products products = storeService.loadProductsFromFile(PRODUCT_FILE_PATH);
+        view.printCurrentProducts(products);
 
-    private void printStartStatement() {
-        outputView.printHello();
-    }
-
-    private void printCurrentProducts(List<Product> products) {
-        outputView.printCurrentProducts(products);
+        String productSelect = view.requestProductSelect();
     }
 }
