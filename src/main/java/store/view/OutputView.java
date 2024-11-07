452fa38852cb28a3d64947c12924d00b0f105ca4
IM-GYURI <kgulr0517@gmail.com>
2024-11-08 00:46:00 +0900

    feat : 현재 보유하고 있는 상품 목록을 출력하는 기능을 구현한다.
    - printEachProduct() : 각 상품 객체를 포맷팅한 후 출력
    - formatProduct() : 각 상품 객체를 PRODUCT_FORMAT에 맞춰 이름과 가격을 포맷팅한 후, 수량과 프로모션에 대해 처리함
    - formatNumber() : DecimalFormat을 사용하여 1,000과 같이 3자리 단위로 포맷팅
    - formatQuantity() : 수량이 0보다 크면 formatNumber()를 사용하여 1,000과 같이 포맷팅, 그렇지 않다면 "재고 없음"
    - formatPromotion() : 프로모션이 존재하면 프로모션 정보를 반환, 그렇지 않다면 ""

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,17 +1,59 @@
 package store.view;
 
+import java.text.DecimalFormat;
+import java.util.List;
+import store.domain.Product;
+
 public class OutputView {
 
     private static final String HELLO_STATEMENT = "안녕하세요. W편의점입니다.";
     private static final String CURRENT_PRODUCTS_STATEMENT = "현재 보유하고 있는 상품입니다.";
+    private static final String NUMBER_FORMAT = "#,###";
+    private static final String PRODUCT_FORMAT = "- %s %s원 ";
+    private static final String QUANTITY_FORMAT = "%s개 ";
+    private static final String OUT_OF_STOCK = "재고 없음";
+    private static final String BLANK = "";
     private static final String NEW_LINE = System.lineSeparator();
 
     public void printHello() {
         System.out.println(HELLO_STATEMENT);
     }
 
-    public void printCurrentProducts() {
+    public void printCurrentProducts(List<Product> products) {
         System.out.println(CURRENT_PRODUCTS_STATEMENT + NEW_LINE);
-        // 상품 목록 출력
+        printEachProduct(products);
+    }
+
+    private void printEachProduct(List<Product> products) {
+        products.stream()
+                .map(this::formatProduct)
+                .forEach(System.out::println);
+    }
+
+    private String formatProduct(Product product) {
+        return String.format(PRODUCT_FORMAT, product.getName(), formatNumber(product.getPrice()))
+                + formatQuantity(product)
+                + formatPromotion(product);
+    }
+
+    private String formatNumber(int number) {
+        DecimalFormat decimalFormat = new DecimalFormat(NUMBER_FORMAT);
+        return decimalFormat.format(number);
+    }
+
+    private String formatQuantity(Product product) {
+        if (product.getQuantity() > 0) {
+            return String.format(QUANTITY_FORMAT, formatNumber(product.getQuantity()));
+        }
+
+        return OUT_OF_STOCK;
+    }
+
+    private String formatPromotion(Product product) {
+        if (product.getPromotion() != null) {
+            return product.getPromotion().getName();
+        }
+
+        return BLANK;
     }
 }
