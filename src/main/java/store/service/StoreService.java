07ed5b07464c522745bcfb9cf4a410fe4dc0b8db
IM-GYURI <kgulr0517@gmail.com>
2024-11-08 03:05:38 +0900

    feat : StoreService가 PurchaseItemParser를 사용하여 parsePurchaseItems()를 구현한다.

--- src/main/java/store/service/StoreService.java
+++ src/main/java/store/service/StoreService.java
@@ -1,17 +1,26 @@
 package store.service;
 
+import java.util.List;
 import store.domain.Products;
+import store.domain.PurchaseItem;
 import store.util.ProductLoader;
+import store.util.PurchaseItemParser;
 
 public class StoreService {
 
     private final ProductLoader productLoader;
+    private final PurchaseItemParser purchaseItemParser;
 
-    public StoreService(ProductLoader productLoader) {
+    public StoreService(ProductLoader productLoader, PurchaseItemParser purchaseItemParser) {
         this.productLoader = productLoader;
+        this.purchaseItemParser = purchaseItemParser;
     }
 
     public Products loadProductsFromFile(String productFilePath) {
         return productLoader.loadProducts(productFilePath);
+    }
+
+    public List<PurchaseItem> parsePurchaseItems(String inputStatement, Products products) {
+        return purchaseItemParser.parsePurchaseItems(inputStatement, products);
     }
 }
