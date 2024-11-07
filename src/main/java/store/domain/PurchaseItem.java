5a11e1f0e3fa451533d5e5b253f97742a399ba4f
IM-GYURI <kgulr0517@gmail.com>
2024-11-08 03:06:52 +0900

    feat : 구매할 상품명과 수량을 가지는 PurchaseItem 도메인을 구현한다.

---
+++ src/main/java/store/domain/PurchaseItem.java
@@ -0,0 +1,12 @@
+package store.domain;
+
+public class PurchaseItem {
+
+    private String name;
+    private int quantity;
+
+    public PurchaseItem(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
