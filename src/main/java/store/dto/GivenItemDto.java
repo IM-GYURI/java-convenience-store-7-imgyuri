9033b2873023acba4e95e09eecf031f3eed60f53
IM-GYURI <kgulr0517@gmail.com>
2024-11-11 06:15:15 +0900

    refactor : GivenItemDto를 생성하는 from()을 추가한다.
    - from() : PurchaseItem으로부터 GivenItemDto를 생성
    - calculateFreeQuantity() : 증정 수량 계산

--- src/main/java/store/dto/GivenItemDto.java
+++ src/main/java/store/dto/GivenItemDto.java
@@ -1,7 +1,27 @@
 package store.dto;
 
+import store.domain.Product;
+import store.domain.PurchaseItem;
+
 public record GivenItemDto(
         String name,
         int freeQuantity,
         int price) {
+
+    public static GivenItemDto from(PurchaseItem purchaseItem) {
+        Product product = purchaseItem.getProduct();
+        int freeQuantity = calculateFreeQuantity(purchaseItem);
+
+        return new GivenItemDto(product.name(), freeQuantity, product.price());
+    }
+
+    private static int calculateFreeQuantity(PurchaseItem purchaseItem) {
+        Product product = purchaseItem.getProduct();
+
+        if (product.promotion() != null) {
+            return product.promotion().calculateFreeQuantity(product.stock().getPromotionStock());
+        }
+
+        return 0;
+    }
 }
