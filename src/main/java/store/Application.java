09d1d8b9392923c5593d67b1d54307d7430abc9f
IM-GYURI <kgulr0517@gmail.com>
2024-11-11 02:45:31 +0900

    refactor : StoreController의 buyProduct()의 함수명을 enterStore()로 변경한다.

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -6,6 +6,6 @@ import store.controller.StoreController;
 public class Application {
     public static void main(String[] args) {
         StoreController storeController = AppConfig.INSTANCE.storeController();
-        storeController.buyProducts();
+        storeController.enterStore();
     }
 }
