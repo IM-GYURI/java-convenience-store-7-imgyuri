3c6968ace817ddd9384abb12689ae1f66fb7fd33
IM-GYURI <kgulr0517@gmail.com>
2024-11-11 02:50:49 +0900

    refactor : Products 도메인을 class에서 record로 변경한다.

--- src/main/java/store/domain/Products.java
+++ src/main/java/store/domain/Products.java
@@ -2,20 +2,6 @@ package store.domain;
 
 import java.util.List;
 
-public class Products {
-
-    private final List<Product> products;
-
-    public Products(List<Product> products) {
-        this.products = products;
-    }
-
-    public boolean existsProduct(String productName) {
-        return products.stream()
-                .anyMatch(product -> product.getName().equals(productName));
-    }
-
-    public List<Product> getProducts() {
-        return products;
-    }
+public record Products(
+        List<Product> products) {
 }
