11c5a0b0258e3c24b4dd8d74ab8a54e722e78b46
IM-GYURI <kgulr0517@gmail.com>
2024-11-11 05:29:56 +0900

    refactor : CommonInputValidator를 InputView로 재통합한다.
    - InputView에서만 사용

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -2,9 +2,13 @@ package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import store.domain.Product;
-import store.validation.CommonInputValidator;
+import store.exception.ErrorMessage;
+import store.exception.ValidatorBuilder;
 
 public class InputView {
+
+    private static final String YES_ANSWER = "Y";
+    private static final String NO_ANSWER = "N";
 
     public String requestProductSelect() {
         System.out.println(Statement.PRODUCT_SELECT_STATEMENT.message);
@@ -14,19 +18,19 @@ public class InputView {
     public String askAdditionalPurchase(Product product) {
         System.out.printf(
                 Statement.NEW_LINE.message + Statement.ADDITIONAL_PURCHASE_FORMAT.message + Statement.NEW_LINE.message,
-                product.getName(), product.getPromotion().getGet());
+                product.name(), product.promotion().getGet());
         String userInput = Console.readLine();
 
-        CommonInputValidator.validateInput(userInput);
+        validateInput(userInput);
         return userInput;
     }
 
     public String askNoPromotion(Product product, int shortageQuantity) {
         System.out.printf(Statement.NEW_LINE.message + Statement.NO_PROMOTION_STATEMENT.message
-                + Statement.NEW_LINE.message, product.getName(), shortageQuantity);
+                + Statement.NEW_LINE.message, product.name(), shortageQuantity);
         String userInput = Console.readLine();
 
-        CommonInputValidator.validateInput(userInput);
+        validateInput(userInput);
         return userInput;
     }
 
@@ -34,7 +38,15 @@ public class InputView {
         System.out.println(Statement.NEW_LINE.message + Statement.MEMBERSHIP_STATEMENT.message);
         String userInput = Console.readLine();
 
-        CommonInputValidator.validateInput(userInput);
+        validateInput(userInput);
         return userInput;
+    }
+
+    private void validateInput(String userInput) {
+        ValidatorBuilder.from(userInput)
+                .validate(input -> input == null || input.isBlank(), ErrorMessage.INPUT_NOT_EMPTY)
+                .validate(input -> !input.equals(YES_ANSWER) && !input.equals(
+                        NO_ANSWER), ErrorMessage.INVALID_YN_INPUT)
+                .get();
     }
 }
