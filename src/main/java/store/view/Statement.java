4c3f1e3a1ac5b9eeb5ca9d18f901d95436369d08
IM-GYURI <kgulr0517@gmail.com>
2024-11-11 05:55:13 +0900

    feat : Statement에 영수증 출력 관련 문구들을 추가한다.

--- src/main/java/store/view/Statement.java
+++ src/main/java/store/view/Statement.java
@@ -12,6 +12,16 @@ public enum Statement {
     ADDITIONAL_PURCHASE_FORMAT("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"),
     NO_PROMOTION_STATEMENT("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)"),
     MEMBERSHIP_STATEMENT("멤버십 할인을 받으시겠습니까? (Y/N)"),
+    START_OF_RECEIPT("==============W 편의점================"),
+    HEADER_OF_PURCHASE("상품명\t\t수량\t금액"),
+    PURCHASE_FORMAT("%s\t\t%s \t%s"),
+    HEADER_OF_GIVEN("=============증\t정==============="),
+    GIVEN_FORMAT("%s\t\t%s"),
+    DIVIDE_LINE("===================================="),
+    TOTAL_PRICE_FORMAT("총구매액\t\t%s\t%s"),
+    PROMOTION_DISCOUNT_FORMAT("행사할인\t\t\t%s"),
+    MEMBERSHIP_DISCOUNT_FORMAT("멤버십할인\t\t\t%s"),
+    PAY_PRICE_FORMAT("내실돈\t\t\t %s"),
     BLANK(""),
     NEW_LINE(System.lineSeparator());
 
