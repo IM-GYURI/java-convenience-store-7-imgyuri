002c285f4e84deb8aca059c83491918713951156
IM-GYURI <kgulr0517@gmail.com>
2024-11-11 07:18:30 +0900

    refactor : 파라미터 이름을 totalPrice가 아닌 currentPrice로 변경한다.
    - 총 금액이 아닌 프로모션 할인 후 가격에 대한 파라미터

--- src/main/java/store/util/MembershipCalculator.java
+++ src/main/java/store/util/MembershipCalculator.java
@@ -9,8 +9,8 @@ public class MembershipCalculator {
     private static final BigDecimal DISCOUNT_ROUNDING_UNIT = BigDecimal.valueOf(1_000);
     private static final int MAX_DISCOUNT_PRICE = 8_000;
 
-    public static int calculateMembershipDiscount(int totalPrice) {
-        BigDecimal discount = BigDecimal.valueOf(totalPrice).multiply(DISCOUNT_RATE);
+    public static int calculateMembershipDiscount(int currentPrice) {
+        BigDecimal discount = BigDecimal.valueOf(currentPrice).multiply(DISCOUNT_RATE);
         discount = discount.divide(DISCOUNT_ROUNDING_UNIT, 0, RoundingMode.DOWN)
                 .multiply(DISCOUNT_ROUNDING_UNIT);
         return discount.min(BigDecimal.valueOf(MAX_DISCOUNT_PRICE)).intValue();
