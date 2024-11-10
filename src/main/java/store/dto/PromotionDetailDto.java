3be9491f7b5e9a75705eb1bf92260de81a0e10fe
IM-GYURI <kgulr0517@gmail.com>
2024-11-11 06:15:36 +0900

    refactor : PromotionDetailDto를 생성하는 from()을 추가한다.
    - from() : PurchaseItem으로부터 PromotionDetailDto 생성

--- src/main/java/store/dto/PromotionDetailDto.java
+++ src/main/java/store/dto/PromotionDetailDto.java
@@ -1,6 +1,20 @@
 package store.dto;
 
+import store.domain.Product;
+import store.domain.PurchaseItem;
+
 public record PromotionDetailDto(
         int promotionQuantity,
         int remainingQuantity) {
+
+    public static PromotionDetailDto from(PurchaseItem purchaseItem) {
+        Product product = purchaseItem.getProduct();
+
+        if (product.promotion() != null) {
+            return product.promotion()
+                    .calculatePromotionAndFree(purchaseItem.getQuantity(), product.stock().getPromotionStock());
+        }
+
+        return new PromotionDetailDto(0, product.stock().getRegularStock());
+    }
 }
