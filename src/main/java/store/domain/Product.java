c2478f2d90629cddb52a3f67ab67152956fc2190
IM-GYURI <kgulr0517@gmail.com>
2024-11-11 05:32:32 +0900

    refactor : Product 도메인을 class에서 record로 변경한다.

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -1,39 +1,8 @@
 package store.domain;
 
-public class Product {
-
-    private String name;
-    private int price;
-    private Stock stock;
-    private Promotion promotion;
-
-    public Product(String name, int price, Stock stock, Promotion promotion) {
-        this.name = name;
-        this.price = price;
-        this.stock = stock;
-        this.promotion = promotion;
-    }
-
-    public int calculateRegularQuantity(int requestedQuantity) {
-        int promotionStock = stock.getPromotionStock();
-        int availableForPromotion = Math.min(promotionStock, requestedQuantity);
-
-        return requestedQuantity - availableForPromotion;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public Stock getStock() {
-        return stock;
-    }
-
-    public Promotion getPromotion() {
-        return promotion;
-    }
+public record Product(
+        String name,
+        int price,
+        Stock stock,
+        Promotion promotion) {
 }
