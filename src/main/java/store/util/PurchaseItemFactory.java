239312019d7daaa331e5d1e75525c752b3b107af
IM-GYURI <kgulr0517@gmail.com>
2024-11-11 02:53:20 +0900

    refactor : ParsedItem의 클래스명을 ParsedItemDto로 변경한다.

--- src/main/java/store/util/PurchaseItemFactory.java
+++ src/main/java/store/util/PurchaseItemFactory.java
@@ -3,18 +3,18 @@ package store.util;
 import store.domain.Product;
 import store.domain.Products;
 import store.domain.PurchaseItem;
-import store.dto.ParsedItem;
+import store.dto.ParsedItemDto;
 import store.exception.ErrorMessage;
 
 public class PurchaseItemFactory {
 
-    public static PurchaseItem createPurchaseItem(ParsedItem parsedItem, Products products) {
-        Product product = findMatchProduct(parsedItem.name(), products);
-        return new PurchaseItem(product, parsedItem.quantity());
+    public static PurchaseItem createPurchaseItem(ParsedItemDto parsedItemDto, Products products) {
+        Product product = findMatchProduct(parsedItemDto.name(), products);
+        return new PurchaseItem(product, parsedItemDto.quantity());
     }
 
     private static Product findMatchProduct(String name, Products products) {
-        return products.getProducts().stream()
+        return products.products().stream()
                 .filter(product -> product.getName().equals(name))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.PRODUCT_NOT_EXISTS.name()));
