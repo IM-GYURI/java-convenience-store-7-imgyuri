64334c37f1208372e2bdd21e7958760ed89f05cf
IM-GYURI <kgulr0517@gmail.com>
2024-11-11 05:54:51 +0900

    feat : ReceiptDto에 총 수량을 추가한다.

--- src/main/java/store/dto/ReceiptDto.java
+++ src/main/java/store/dto/ReceiptDto.java
@@ -5,6 +5,7 @@ import java.util.List;
 public record ReceiptDto(
         List<PurchasedDto> purchasedItems,
         List<GivenItemDto> givenItems,
+        int totalQuantity,
         int totalPrice,
         int promotionDiscountPrice,
         int membershipDiscountPrice,
