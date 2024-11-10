0863df621a88b7fc222b4cd3b494ff3091241584
IM-GYURI <kgulr0517@gmail.com>
2024-11-10 16:52:29 +0900

    feat : 에러메세지를 추가한다.
    - STOCK_SHORTAGE("구매 수량에 비해 재고가 부족한 상품이 포함되어 있습니다.")

--- src/main/java/store/exception/ErrorMessage.java
+++ src/main/java/store/exception/ErrorMessage.java
@@ -6,6 +6,7 @@ public enum ErrorMessage {
     PURCHASE_ITEM_DUPLICATED("상품명이 중복되었습니다."),
     PRODUCT_NOT_EXISTS("해당 상품이 존재하지 않습니다."),
     INVALID_QUANTITY("수량은 1 이상이어야 합니다."),
+    STOCK_SHORTAGE("구매 수량에 비해 재고가 부족한 상품이 포함되어 있습니다."),
     PURCHASE_QUANTITY_NOT_INTEGER("구매 수량은 정수만 가능합니다."),
     INVALID_YN_INPUT("입력값은 'Y' 또는 'N'이어야 합니다.");
 
