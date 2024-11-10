a6413993917845223313c182843817d03850d8fc
IM-GYURI <kgulr0517@gmail.com>
2024-11-10 16:25:52 +0900

    feat : InputView에 추가 구매를 묻고, 입력에 대해 검증한 후 문자열을 반환하는 기능을 구현한다.
    - askAdditionalPurchase() : 추가 구매 여부 묻기
    - validateInput() : null이나 공백이 아닌지, Y나 N로 대답했는지 검증

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,11 +1,31 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import store.domain.Product;
+import store.exception.ErrorMessage;
+import store.exception.ValidatorBuilder;
 
 public class InputView {
 
     public String requestProductSelect() {
         System.out.println(Statement.PRODUCT_SELECT_STATEMENT.message);
         return Console.readLine();
+    }
+
+    public String askAdditionalPurchase(Product product) {
+        System.out.printf(Statement.ADDITIONAL_PURCHASE_FORMAT.message + Statement.NEW_LINE.message,
+                product.getName(), product.getPromotion().getGet());
+        String userInput = Console.readLine();
+
+        validateInput(userInput);
+        return userInput;
+    }
+
+    private void validateInput(String userInput) {
+        ValidatorBuilder.from(userInput)
+                .validate(input -> input == null || input.isBlank(), ErrorMessage.INPUT_NOT_EMPTY)
+                .validate(input -> !input.equals(Statement.YES_ANSWER.message) && !input.equals(
+                        Statement.NO_ANSWER.message), ErrorMessage.INVALID_YN_INPUT)
+                .get();
     }
 }
