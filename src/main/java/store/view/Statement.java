e8a9d85de1e9e6a4f332dd1900480f257b247e77
IM-GYURI <kgulr0517@gmail.com>
2024-11-11 00:13:32 +0900

    refactor : Y와 N을 CommonInputValidator로 이동시킨다.

--- src/main/java/store/view/Statement.java
+++ src/main/java/store/view/Statement.java
@@ -10,8 +10,7 @@ public enum Statement {
     QUANTITY_FORMAT("%s개 "),
     OUT_OF_STOCK("재고 없음 "),
     ADDITIONAL_PURCHASE_FORMAT("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"),
-    YES_ANSWER("Y"),
-    NO_ANSWER("N"),
+    NO_PROMOTION_STATEMENT("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)"),
     BLANK(""),
     NEW_LINE(System.lineSeparator());
 
