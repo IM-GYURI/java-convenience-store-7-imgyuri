c39a3ed9bfebb6e46691c711ee658cff98ef2661
IM-GYURI <kgulr0517@gmail.com>
2024-11-11 05:32:13 +0900

    feat : 영수증을 출력하는 기능을 구현한다.
    printReceipt()
    - 영수증 전체 출력
    
    printPurchases()
    - 구매 항목 전체 출력
    
    printEachPurchase()
    - 구매 항목 하나씩 출력
    
    printGivenItems()
    - 증정 항목 전체 출력
    
    printEachGivenItem()
    - 증정 항목 하나씩 출력
    
    printFooter()
    - 영수증의 하단 부분 출력
    - 총구매액, 행사할인, 멤버십할인, 내실돈

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -4,8 +4,17 @@ import java.text.DecimalFormat;
 import java.util.List;
 import store.domain.Product;
 import store.domain.Products;
+import store.dto.GivenItemDto;
+import store.dto.PurchasedDto;
+import store.dto.ReceiptDto;
 
 public class OutputView {
+
+    private static final int MINUS_FOR_DISCOUNT = -1;
+
+    public static void printErrorMessage(String errorMessage) {
+        System.out.println(errorMessage + Statement.NEW_LINE.message);
+    }
 
     public void printHello() {
         System.out.println(Statement.HELLO_STATEMENT.message);
@@ -16,8 +25,16 @@ public class OutputView {
         printEachProduct(products.products());
     }
 
-    public static void printErrorMessage(String errorMessage) {
-        System.out.println(errorMessage + Statement.NEW_LINE.message);
+    public void printReceipt(ReceiptDto receiptDto) {
+        System.out.println(Statement.START_OF_RECEIPT.message);
+        System.out.println(Statement.HEADER_OF_PURCHASE.message);
+        printPurchases(receiptDto.purchasedItems());
+
+        System.out.println(Statement.HEADER_OF_GIVEN.message);
+        printGivenItems(receiptDto.givenItems());
+        System.out.println(Statement.DIVIDE_LINE.message);
+
+        printFooter(receiptDto);
     }
 
     private void printEachProduct(List<Product> products) {
@@ -26,19 +43,51 @@ public class OutputView {
     }
 
     private void printFormattedProduct(Product product) {
-        if (product.getPromotion() != null) {
-            System.out.println(
-                    formatProduct(product, product.getStock().getPromotionStock(), product.getPromotion().getName()));
-            System.out.println(formatProduct(product, product.getStock().getRegularStock(), Statement.BLANK.message));
+        if (product.promotion() != null) {
+            System.out.println(formatProduct(product
+                    , product.stock().getPromotionStock(), product.promotion().getName()));
+            System.out.println(formatProduct(product, product.stock().getRegularStock(), Statement.BLANK.message));
             return;
         }
 
-        System.out.println(formatProduct(product, product.getStock().getRegularStock(), Statement.BLANK.message));
+        System.out.println(formatProduct(product, product.stock().getRegularStock(), Statement.BLANK.message));
+    }
+
+    private void printPurchases(List<PurchasedDto> purchasedItems) {
+        purchasedItems.forEach(this::printEachPurchase);
+    }
+
+    private void printEachPurchase(PurchasedDto purchasedDto) {
+        System.out.printf(Statement.PURCHASE_FORMAT.message + Statement.NEW_LINE.message
+                , purchasedDto.name(), formatNumber(purchasedDto.quantity())
+                , formatNumber(purchasedDto.totalPrice()));
+    }
+
+    private void printGivenItems(List<GivenItemDto> givenItems) {
+        givenItems.forEach(this::printEachGivenItem);
+    }
+
+    private void printEachGivenItem(GivenItemDto givenItemDto) {
+        if (givenItemDto.freeQuantity() != 0) {
+            System.out.printf(Statement.GIVEN_FORMAT.message + Statement.NEW_LINE.message
+                    , givenItemDto.name(), formatNumber(givenItemDto.freeQuantity()));
+        }
+    }
+
+    private void printFooter(ReceiptDto receiptDto) {
+        System.out.printf(Statement.TOTAL_PRICE_FORMAT.message + Statement.NEW_LINE.message,
+                receiptDto.totalQuantity(), formatNumber(receiptDto.totalPrice()));
+        System.out.printf(Statement.PROMOTION_DISCOUNT_FORMAT.message + Statement.NEW_LINE.message
+                , formatNumber(MINUS_FOR_DISCOUNT * receiptDto.promotionDiscountPrice()));
+        System.out.printf(Statement.MEMBERSHIP_DISCOUNT_FORMAT.message + Statement.NEW_LINE.message
+                , formatNumber(MINUS_FOR_DISCOUNT * receiptDto.membershipDiscountPrice()));
+        System.out.printf(Statement.PAY_PRICE_FORMAT.message + Statement.NEW_LINE.message
+                , formatNumber(receiptDto.payPrice()));
     }
 
     private String formatProduct(Product product, int stock, String promotion) {
         return String.format(Statement.PRODUCT_FORMAT.message
-                , product.getName(), formatNumber(product.getPrice()))
+                , product.name(), formatNumber(product.price()))
                 + formatQuantity(stock)
                 + formatPromotion(promotion);
     }
