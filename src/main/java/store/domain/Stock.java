4b7e1646e409baf76c370659635ec907127016fd
IM-GYURI <kgulr0517@gmail.com>
2024-11-10 16:36:03 +0900

    feat : 프로모션 재고와 일반 재고를 관리하는 Stock 도메인을 추가 구현한다.

---
+++ src/main/java/store/domain/Stock.java
@@ -0,0 +1,28 @@
+package store.domain;
+
+public class Stock {
+
+    private int promotionStock;
+    private int regularStock;
+
+    public Stock(int promotionStock, int regularStock) {
+        this.promotionStock = promotionStock;
+        this.regularStock = regularStock;
+    }
+
+    public int getRegularStock() {
+        return regularStock;
+    }
+
+    public int getPromotionStock() {
+        return promotionStock;
+    }
+
+    public void setRegularStock(int regularStock) {
+        this.regularStock = regularStock;
+    }
+
+    public void setPromotionStock(int promotionStock) {
+        this.promotionStock = promotionStock;
+    }
+}
