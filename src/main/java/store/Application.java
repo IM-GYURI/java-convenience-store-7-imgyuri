a4bdd86a6b0a5d4a58aef7d3e0cda7079ab447c3
IM-GYURI <kgulr0517@gmail.com>
2024-11-11 18:54:23 +0900

    feat : Application의 main()을 try-catch로 처리한다.
    - 입력 횟수 제한인 5회를 넘게 될 경우 던져지는 에러 처리
    - 에러메세지 출력 후 프로그램 종료

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -2,10 +2,15 @@ package store;
 
 import store.config.AppConfig;
 import store.controller.StoreController;
+import store.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        StoreController storeController = AppConfig.INSTANCE.storeController();
-        storeController.enterStore();
+        try {
+            StoreController storeController = AppConfig.INSTANCE.storeController();
+            storeController.enterStore();
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+        }
     }
 }
